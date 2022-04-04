dce9c6ea7f490f4c93f768745cea6fbc3c6456c2
shoaibahmadsakib <shoaibahmadsakib@gmail.com>
2022-04-05 02:33:55 +0600

    route added in applicaation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Header from "./Components/Header";
+import Home from "./Pages/Home";
+import Prices from "./Pages/Prices";
+import Services from "./Pages/Services";
 
 function App() {
   return (
     <div className="App">
-     <h1 className="text-lg p-10 text-red-300">sakib</h1>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/services" element={<Services/>}></Route>
+        <Route path="/prices" element={<Prices/>}></Route>
+      </Routes>
     </div>
   );
 }
