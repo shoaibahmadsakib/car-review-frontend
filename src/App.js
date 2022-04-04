82570c4025ca2d2e5374e1ece94caf1455d6c621
shoaibahmadsakib <shoaibahmadsakib@gmail.com>
2022-04-05 02:01:24 +0600

    add every framework,taliwind,router

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>setup react</h1>
+     <h1 className="text-lg p-10 text-red-300">sakib</h1>
     </div>
   );
 }
